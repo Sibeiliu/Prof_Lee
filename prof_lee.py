94ac472e4103e72c5f1441e0bb3ddf71fe94e250
Sibei Liu <sl4660@cumc.columbia.edu>
2021-02-14 11:33:03 -0500

    updata

--- prof_lee.py
+++ prof_lee.py
@@ -4,4 +4,53 @@ Created on Fri Feb 12 17:26:41 2021
 
 @author: Sibei
 """
-## to hard to use python without knowing it sad
+## push python into github
+# 1) git init
+# 2) git add . or git add ['filename']
+# 3) git commit -m "My first File"
+# 6) git push origin master
+
+
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="1" 
+
+
+
+import keras
+from matplotlib import pyplot as plt
+import numpy as np
+import gzip
+%matplotlib inline
+from keras.layers import Input,Conv2D,MaxPooling2D,UpSampling2D
+from keras.models import Model
+from keras.optimizers import RMSprop
+
+# import nii data
+
+from nibabel.testing import data_path
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
